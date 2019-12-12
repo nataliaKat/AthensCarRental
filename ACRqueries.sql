a6113ca6b3e7e34fa3ab1cbe404e8664cfca802e
Natalia Katsiapi <kat26natalia@gmail.com>
2019-12-12 23:44:54 +0200

    Correct instructions in greek

--- ACRqueries.sql
+++ ACRqueries.sql
@@ -1,41 +1,44 @@
-use DB30
+use DB30;
 
-/* 1) Äåßîå ìßá ëßóôá ôùí ðåëáôþí ìå ôïí êùäéêü ôïõò, ôï åðþíõìü ôïõò, ôç äéåýèõíóÞ ôïõò êáé ôï ôçëÝöùíü ôïõò. */ 	
+/* 1)  Δείξε μία λίστα των πελατών με τον κωδικό τους, το επώνυμό τους, τη διεύθυνσή τους και το τηλέφωνό τους. */
 select customer_id, last_name, address, phonenumber
 from Customer;
 
-/* 2) Äåßîå ãéá êÜèå åíïéêßáóç ôïí êùäéêü ôçò êáé ôï ÷ñïíéêü äéÜóôçìÜ ôçò (áðü, Ýùò), åÜí ç áîßá åßíáé ðÜíù áðü 200 */
+/* 2) Δείξε για κάθε ενοικίαση τον κωδικό της και το χρονικό διάστημά της (από, έως) εάν η αξία είναι πάνω από 200€. */
 select rental_id, start_date, end_date 
 from Rental, Payment
 where Rental.verification_number = Payment.verification_number and cost > 200;
 
-/* 3) Ãéá êÜèå ðåëÜôç, äåßîå ôïí êùäéêü ôïõ, ôï ïíïìáôåðþíõìü ôïõ, ôï ôçëÝöùíü ôïõ êáé ôïõò êùäéêïýò ôùí åíïéêéÜóåùí ðïõ Ý÷åé êÜíåé*/
+/* 3) Για κάθε πελάτη, δείξε τον κωδικό του, το ονοματεπώνυμό του, το τηλέφωνό του και τους κωδικούς των ενοικιάσεων
+	  που έχει κάνει */
 select Customer.customer_id, first_name, last_name, phonenumber, rental_id
 from Customer, Rental
 where Customer.customer_id = Rental.customer_id;
 
-/* 4) Äåßîå ôï ïíïìáôåðþíõìï êáé ôçëÝöùíï ôùí ðåëáôþí ðïõ åß÷áí óôçí êáôï÷Þ ôïõò áõôïêßíçôï ôçí 23/9/2010 êáé ðñïÝñ÷ïíôáé áðü ãåùãñáöéêÞ ðåñéï÷Þ ìå êùäéêü 10025 */
+/* 4) Δείξε το ονοματεπώνυμο και τηλέφωνο των πελατών που είχαν στην κατοχή τους αυτοκίνητο την 23/9/2010 και προέρχονται
+	  από γεωγραφική περιοχή με κωδικό 10025. */
 select first_name, last_name, phonenumber
 from Customer, Rental
 where Customer.customer_id = Rental.customer_id and '2010-09-23' >= start_date and '2010-09-23' <= end_date and reg_id = 10025;
 
-/* 5) Ìåßùóå ôçí áîßá üëùí ôùí åíïéêéÜóåùí êáôÜ 5% */
+/* 5) Μείωσε την αξία όλων των ενοικιάσεων κατά 5% */
 update Payment
 set cost = cost * 0.95;
 
-/* 6) Äåßîå ãéá êÜèå ìÞíá ôïõ 2010 ôï óýíïëï êáé ôï ìÝóï üñï ôùí ðëçñùìþí ðïõ Ý÷ïõí ãßíåé */
+/* 6) Δείξε για κάθε μήνα του 2010 το σύνολο και το μέσο όρο των πληρωμών που έχουν γίνει. */
 select month(pdate) as month, sum(cost) as total, avg(cost) as average 
 from Payment
 where year(pdate) = '2010'
 group by month(pdate);
 
-/* 7) Äåßîå ôç óõíïëéêÞ áîßá ôùí åíïéêéÜóåùí áíÜ êáôçãïñßá áõôïêéíÞôïõ êáé ãåùãñáöéêü äéáìÝñéóìá */
+/* 7) Δείξε τη συνολική αξία των ενοικιάσεων ανά κατηγορία αυτοκινήτου και γεωγραφικό διαμέρισμα. */
 select cat_id as category, reg_id as region, sum(cost) as total
 from Customer, Payment, Rental, Car
 where Payment.verification_number = Rental.verification_number and Customer.customer_id = Rental.customer_id and Car.vin = Rental.vin
 group by cat_id, reg_id;
 
-/* 8) Äåßîå ôïõò êùäéêïýò ôùí ðåëáôþí ðïõ Ý÷ïõí ãéá ôï ìÞíá Éïýíéï 2010 ðÜíù áðü 4 åíïéêéÜóåéò êáé ç ìÝóç áîßá åíïéêßáóçò Þôáí ðÜíù áðü 150.*/
+/* 8) Δείξε τους κωδικούς των πελατών που έχουν για το μήνα Ιούνιο 2010 πάνω από 4 ενοικιάσεις και η μέση
+      αξία ενοικίασης ήταν πάνω από 150€. */
 go
 create view June_rentals(customer_id, number_of_rentals, average_June) as 
 select customer_id, count(customer_id), avg(cost) 
@@ -49,7 +52,8 @@ select customer_id
 from June_rentals
 where number_of_rentals > 4 and average_june > 150;
 
-/* 9) ×ñçóéìïðïéþíôáò åìöùëåõìÝíá õðïåñùôÞìáôá, äåßîå ôïí êùäéêü êáé ôï ïíåìáôåðþíõìï ôùí ðåëáôþí ðïõ Ý÷ïõí êÜíåé óõíïëéêÝò ðëçñùìÝò ôïí Áðñßëéï ôïõ 2010 ðÜíù áðü 1500. */
+/* 9) Χρησιμοποιώντας εμφωλευμένα υποερωτήματα, δείξτε τον κωδικό και το ονοματεπώνυμο των πελατών που έχουν
+      κάνει συνολικές πληρωμές τον Απρίλιο του 2010 πάνω από 1500€. */
 select Customer.customer_id, first_name, last_name 
 from (select customer_id, sum(cost) as total
 	  from Rental, Payment
@@ -64,7 +68,8 @@ Where 150 < (Select Sum(cost)
              Where year(pdate) = 2010 AND month(pdate) = 4 AND Rental.verification_number = Payment.verification_number AND Customer.customer_id = Rental.customer_id
              Group By customer_id);
 
-/* 10) Ãéá êÜèå êáôçãïñßá áõôïêéíÞôùí, äåßîå ôç óõíïëéêÞ áîßá åíïéêéÜóåùí ôçò êáôçãïñßáò óáí ðïóïóôü ôçò óõíïëéêÞò áîßáò üëùí ôùí åíïéêéÜóåùí. */
+/* 10) Για κάθε κατηγορία αυτοκινήτων, δείξε τη συνολική αξία ενοικιάσεων της κατηγορίας σαν ποσοστό της συνολικής
+	   αξίας όλων των ενοικιάσεων. */
 go
 create view SumCost(total) as 
 select sum(cost)
@@ -75,7 +80,7 @@ from Payment, Rental, Car, SumCost
 where Payment.verification_number = Rental.verification_number and Car.vin = Rental.vin 
 group by cat_id, total;
 
-/* 11) Ãéá êÜèå ìÞíá ôïõ 2011, óýãêñéíå ôéò óõíïëéêÝò ðëçñùìÝò ôïõ ìÞíá ìå áõôÝò ôïõ áíôßóôïé÷ïõ ìÞíá ôïõ 2010 (óáí ðïóïóôü) */
+/* 11) Για κάθε μήνα του 2010, σύγκρινς τις συνολικές πληρωμές του μήνα με αυτές του αντίστοιχου μήνα του 2010 (σαν ποσοστό). */
 go
 create view Payments10(month, total) as
 select month(pdate), sum(cost)
@@ -97,7 +102,8 @@ select Payments11.month, ((Payments11.total - Payments10.total) / Payments10.tot
 from Payments11
 left outer join Payments10 on Payments10.month = Payments11.month;
 
-/* 12) Äåßîå ôïõò êùäéêïýò ôùí ãåùãñáöéêþí äéáìåñéóìÜôùí ðïõ åß÷áí ìÝóç áîßá åíïéêßáóçò ìåãáëýôåñç áðü ôç óõíïëéêÞ ìÝóç áîßá åíïéêßáóçò */
+/* 12) Δείξε τους κωδικούς των γεωγραφικών διαμερισμάτων που είχαν μέση αξία ενοικίασης μεγαλύτερη
+	   από τη συνολική μέση αξία ενοικίασης. */
 select reg_id
 from Payment, Rental, Customer
 where Payment.verification_number = Rental.verification_number and Customer.customer_id = Rental.customer_id
@@ -105,7 +111,8 @@ group by reg_id
 having avg(cost) > (select avg(cost)
 					from Payment);
 
-/* 13) Ãéá êÜèå ìÞíá ìÝôñçóå ðüóïé ðåëÜôåò Ý÷ïõí ìÝóç áîßá åíïéêßáóçò ìåãáëýôåñç áðü ôç óõíïëéêÞ ìÝóç áîßá ôïõ ìÞíá */
+/* 13) Για κάθε μήνα, μέτρησε πόσοι πελάτες έχουν μέση αξία ενοικίασης μεγαλύτερη από τη συνολική
+	   μέση αξία του μήνα. */
 go 
 create view Avg_Month(month, average) as
 select month(pdate), avg(cost)
@@ -124,7 +131,8 @@ from AvgCustMonth, Avg_Month
 where Avg_Month.month = AvgCustMonth.month and AvgCustMonth.average > Avg_Month.average
 group by Avg_Month.month;
 
-/* 14) Ãéá êÜèå ìÞíá ôïõ 2010, äåßîå ôç ìÝóç ÷ñïíéêÞ äéÜñêåéá åíïéêßáóçò (óå çìÝñåò). Èåùñåßóôå üôé ìßá åíïéêßáóç áíÞêåé óôï ìÞíá åêåßíï óôïí ïðïßï áñ÷ßæåé. */
+/* 14) Για κάθε μήνα του 2010, δείξε τη μέση χρονική διάρκεια ενοικίασης (σε ημέρες).
+	   Θεωρείστε ότι μία ενοικίαση ανοίκει στο μήνα εκείνο στον οποίο αρχίζει.*/
 go
 create view Dur(month, duration) as
 select month(start_date), datediff(day, start_date, end_date)
