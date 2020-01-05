42ef3d17794abb6690138aad9c10bba17be6b3f7
Natalia Katsiapi <kat26natalia@gmail.com>
2020-01-05 19:00:52 +0200

    Just change println to print (all fine)

--- ACRJava/src/Main.java
+++ ACRJava/src/Main.java
@@ -15,13 +15,13 @@ public class Main {
 			return;
 		}
 		dao.printRentalsPerCustomer(id);
-		System.out.println("Enter rental id: ");
+		System.out.print("Enter rental id: ");
         try {
 			id = in.nextInt();
-            } catch (InputMismatchException e) {
-				System.out.println("I expected a number");
-				return;
-            }
+        } catch (InputMismatchException e) {
+			System.out.println("I expected a number");
+		    return;
+		}
 		dao.deleteRental(id);
 	}
 
